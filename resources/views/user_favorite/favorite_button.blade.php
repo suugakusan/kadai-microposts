2b029465f06876c0ba722cc54b0e7a0fea13be42
suugakusan <rubypra0804@gmail.com>
2021-11-09 21:15:58 +0000

    一覧画面に追加

--- resources/views/user_favorite/favorite_button.blade.php
+++ resources/views/user_favorite/favorite_button.blade.php
@@ -1,11 +1,11 @@
     @if (Auth::user()->is_favoriting($micropost->id))
         {{-- お気に入りボタンのフォーム --}}
         {!! Form::open(['route' => ['favorites.unfavorite', $micropost->id], 'method' => 'delete']) !!}
-            {!! Form::submit('Unfavorite', ['class' => "btn btn-danger btn-sm"]) !!}
+            {!! Form::submit('Unfavorite', ['class' => "btn btn-success btn-sm"]) !!}
         {!! Form::close() !!}
     @else
         {{-- お気に入りボタンのフォーム --}}
         {!! Form::open(['route' => ['favorites.favorite', $micropost->id]]) !!}
-            {!! Form::submit('Favorite', ['class' => "btn btn-primary btn-sm"]) !!}
+            {!! Form::submit('Favorite', ['class' => "btn btn-light btn-sm"]) !!}
         {!! Form::close() !!}
     @endif
